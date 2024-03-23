52b4fb326fe6f5e1243c69c6f8002c5c9e9e9e87
noie manoy <noiemanoy.dev@gmail.com>
2024-03-23 13:16:07 +0800

    new api urls1

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ app.use(bodyParser.json({extended: true}));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(cors(
-//     {
-//     origin: 'https://unladkomunidad.onrender.com',
-//     credentials: true
-// }
+    {
+    origin: 'https://unladkomunidad.onrender.com',
+    credentials: true
+}
 ));
 
 app.use("/api/auth", authRoute);
