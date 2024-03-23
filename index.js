6261fe18bb4ce2ca37f139fc5df8347adcba7d0a
noie manoy <noiemanoy.dev@gmail.com>
2024-03-23 09:55:47 +0800

    added origin

--- index.js
+++ index.js
@@ -51,8 +51,8 @@ const connectMongoDB = async () => {
     }
 }
 
-const PORT = process.env.PORT || 8800;
-app.listen(PORT, () => {
-    connectMongoDB();
-    console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 8800;
+// app.listen(PORT, () => {
+//     connectMongoDB();
+//     console.log(`Server is running on port ${PORT}`);
+// });
