a709efa7a68205e8e4aa337a8f09dec060ea967e
noie manoy <noiemanoy.dev@gmail.com>
2024-03-24 10:49:46 +0800

    index

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(cors(
     {
-    origin: 'https://unladkomunidad.onrender.com',
+    origin: 'https://unladkomunidad-2xnq.onrender.com',
     credentials: true
 }
 ));
